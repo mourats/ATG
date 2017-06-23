e3f0373f478c23ca505d22701f5a4ddab3c40caa
Thiago Santos <thiago.moura@ccc.ufcg.edu.br>
2017-06-23 17:27:03 -0300

    AttFinal!

--- Prática 02/pratica_02.py
+++ Prática 02/pratica_02.py
@@ -10,35 +10,60 @@ import graph_functions
 '''
 
 # Código da função 1:
+'''Função responsável por verficar se o passeio válido é uma trilha do grafo.'''
 def is_trail(graph, valid_ride):
-           
+     
+    '''Percorre a lista de arestas recebida.'''
     for edge in valid_ride:
+		
         if type(edge) == tuple:
             inverse_edge = (edge[1] , edge[0])
        
         if type(edge) == str:
-            inverse_edge = (edge[1] + edge[0]) 
-   
+            inverse_edge = (edge[1] + edge[0])
+            
+		            
+	'''Verifica se existe apenas uma aresta com o mesmo conjunto de vértices. Caso contrário, o passeio não é uma trilha.'''
         if (valid_ride.count(edge) + valid_ride.count(inverse_edge)) > 1:
             return False
            
     return True
     
 # Código da função 2:
+'''Função responsável por verficar se o passeio válido é uma trilha de Euler do grafo'''
 def is_trail_euler (grafo,valid_ride):
  
-    temp_edges = graph_functions.edges(grafo)
+	'''Armazena a lista de arestas do grafo'''
+	temp_edges = graph_functions.edges(grafo)
  
-    for edge in valid_ride:
-        a,b = edge
-        if set([a,b]) in temp_edges :
-            temp_edges.remove(set([a,b]))
+	for edge in valid_ride:
+		a,b = edge
+		if set([a,b]) in temp_edges :
+			temp_edges.remove(set([a,b]))
    
-    ran_all_edges = temp_edges == []
-    trail = is_trail(grafo,valid_ride)
+	ran_all_edges = temp_edges == []
+	trail = is_trail(grafo,valid_ride)
    
-    return ran_all_edges and trail
-    
+	''' Se o passeio for uma trilha e passar por todos os vertices do grafo, então é uma trilha de Euler.'''
+	return ran_all_edges and trail
+
+
+# Código da função 3:
+'''Função responsável por retornar a co-árvore da árvore em relação ao grafo recebido'''
+def co_tree(graph,edges_tree):
+	
+	'''Armazena a lista de arestas do grafo'''
+	graph_edges = graph_functions.edges(graph)
+
+	for edge in edges_tree:
+		
+		if type(edge) == str:
+			edge = (edge[0],edge[1])
+			
+		graph_edges.remove(set(edge))
+
+	'''Retorna o conjunto de arestas restantes que representa a co-árvore.'''
+	return graph_edges
  
  
 graph = {"a" : ['b', 'c'],
@@ -49,16 +74,9 @@ graph = {"a" : ['b', 'c'],
          "f" : ['c', 'd', 'g'],
          "g" : ['e', 'f']
         }
-
-'''Falta testar com esse grafo.'''       
-graph2 = {"1" : ['2'],
-          "2" : ['1', '3'],
-          "3" : ['2', '4'],
-          "4" : ['3']
-         }
  
    
-''' Realizando testes com as duas formas de representação de passeios válidos '''
+''' Realizando testes com as duas formas de representação de passeios válidos. '''
 valid_ride1 = ['ac','cf','fd']
 valid_ride2 = ['ed','df','fg','ge','ed']
 valid_ride3 = ['ed','df','fg','ge','eb','ba']
@@ -70,6 +88,13 @@ valid_ride7 = [('e','d'),('d','f'),('f','g'),('g','e'),('e','d')]
 valid_ride8 = [('e','d'),('d','f'),('f','g'),('g','e'),('e','b'),('b','a')]
 valid_ride9 = [('e','d'),('d','f'),('f','g'),('g','e'),('e','b'),('b','a'),('a','c'),('c','f')]
 valid_ride10 = []
+
+arvore1 = [("a","b"),("a","c"),("c","f"),("f","g"),("b","e"),("e","d")]
+arvore2 = [("a","b"),("a","c"),("c","f"),("b","e"),("e","d"),("e","g")]
+
+arvore3 = ["ab","ac","cf","fg","be","ed"]
+arvore4 = ["ab","ac","cf","be","ed","eg"]
+
 
    
 # Testes função 01:            
@@ -99,3 +124,10 @@ assert(is_trail_euler(graph, valid_ride8) == False)
 assert(is_trail_euler(graph, valid_ride9) == True)
 assert(is_trail_euler(graph, valid_ride10) == False)
 
+
+# Testes função 03:
+assert co_tree(graph, arvore1) == [set(['e','g']), set(['d','f'])]
+assert co_tree(graph, arvore2) == [set(['d','f']), set(['f','g'])]
+
+assert co_tree(graph, arvore3) == [set(['e','g']), set(['d','f'])]
+assert co_tree(graph, arvore4) == [set(['d','f']), set(['f','g'])]
